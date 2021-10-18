7aa9299246a1d90901e17896fdfceb1f4c0875d9
Max <shidoutsuruya@gmail.com>
2021-10-18 21:18:42 +0800

    nlp_transfer

--- NLP_transfer.py
+++ NLP_transfer.py
@@ -4,12 +4,13 @@ import nltk
 import re
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
-from itertools import chain
+import joblib 
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 root=r'D:\Python_data\IMDB'
 negative_files_path=os.path.join(root,'train','neg')
 positive_files_path=os.path.join(root,'train','pos')
 unsupervised_files_path=os.path.join(root,'train','unsup')
-CV=CountVectorizer()
+CV=CountVectorizer(lowercase=True)
 def load_data(dir_path):
     lst=[]
     files=os.listdir(dir_path)
@@ -17,21 +18,28 @@ def load_data(dir_path):
         with open(os.path.join(dir_path,file),encoding='utf-8') as f:
             lst.append(f.read())
     return lst
-def corpus_create(stc):
-    stc=re.sub('[^a-zA-Z0-9_ ]','',stc)
-    stc=nltk.word_tokenize(stc.lower())
-    return stc
 negative_data=load_data(negative_files_path)#['hello world','this is my room']
 positive_data=load_data(positive_files_path)
 unsupervised_data=load_data(unsupervised_files_path)
-negative_corpus=[corpus_create(stc) for stc in negative_data]#[['hello','world'],['this'].['is']]
-positive_corpus=[corpus_create(stc) for stc in positive_data]
-
-w2v=CV.fit(negative_data[:5]+positive_data[:5]) 
-print(w2v.vocabulary_)
-
-
-#corpus[['i','was','born'],['he','is','so','good'],]
+if not os.path.exists('cv_model.pkl'):
+    print('start training')
+    cv=CV.fit(negative_data+positive_data)
+    joblib.dump(cv,'cv_model.pkl')
+else:
+    cv=joblib.load('cv_model.pkl')
+    
+def data_preprocess(raw_data,cv=cv,SENTENCE_LENGTH=20):
+    """
+    raw_data:list #['hello world','this is my room']
+    """
+    lst=[]
+    for stc in raw_data:
+        stc=re.sub(r'[^a-zA-Z0-9_ ]','',stc.lower())
+        word_list=nltk.word_tokenize(stc) #['this', 'is', 'my', 'room']
+        word_id_seq=[cv.vocabulary_[item]  for item in word_list if cv.vocabulary_.get(item,0)]#[1,3,4,0,0,0]
+        lst.append(word_id_seq)
+    lst=pad_sequences(lst,maxlen=SENTENCE_LENGTH,padding='post',truncating='post')#keep same length
+    return lst
 #gensim to traing word2vec
 
 """
@@ -41,3 +49,5 @@ similar_word={search_term:[item[0] for item in w2v_model.wv.most_similar([search
 u=pd.DataFrame(similar_word).transpose()
 print(u)
 """
+u=data_preprocess(['hello world','this is my room'])
+print(u)
